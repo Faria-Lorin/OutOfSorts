855e891941f38682c9349496c1bcb5435074c059
Faria-Lorin <florin20@stuy.edu>
2020-12-17 01:21:35 -0500

    I added a second for loop, so that the method repeatedly loops through the whole array, and I added an integer variable, swap, in the first for loop, so that if in the second for loop, if there is a need for swapping, swap increases. If there is no sawpping required inthe second loop, swap stays 0, meaning the array is sorted and the loop should break

--- Sorts.java
+++ Sorts.java
@@ -4,12 +4,19 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    for(int i = 0; i < data.length-1; i++){
-      if (data[i] > data[i+1]){
-        int temp = data[i];
-        data [i] = data[i+1];
-        data [i+1] = temp;
+
+    for(int j = 0; j < data.length-1; j++){
+      int swap = 0;
+      for(int i = 0; i < data.length-1; i++){
+        if (data[i] > data[i+1]){
+          int temp = data[i];
+          data [i] = data[i+1];
+          data [i+1] = temp;
+          swap++;
+        }
+      }
+      if (swap == 0)
+            break;
       }
     }
   }
-}
